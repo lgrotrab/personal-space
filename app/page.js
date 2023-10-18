d0a0e887d4efb1db0d8613d4bc9bbd2a3bca4a6b
Luis Gustavo Rodrigues Oliveira <54413345+lgrotrab@users.noreply.github.com>
2023-10-18 22:15:24 +0000

    feat: adiciona página em construcao

--- app/page.js
+++ app/page.js
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1>Olá sou uma página nova!</h1>;
+  return (
+    <div>
+      <h1>Em construção</h1>
+      <p>Página para postagem de comentários pessoais.</p>
+    </div>
+  );
 }
