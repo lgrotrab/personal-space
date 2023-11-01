3868acc395984a1c16353e5601d412df0add82ab
Luis Gustavo Rodrigues Oliveira <54413345+lgrotrab@users.noreply.github.com>
2023-11-01 14:32:05 +0000

    Adiciona script para criar DB personal e novo user no mongoDB

---
+++ infra/docker-entrypoint-initdb.d/mongo-init.js
@@ -0,0 +1,12 @@
+print("start ~~~~");
+var db = connect(
+  `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@localhost:27017/admin`,
+);
+db = db.getSiblingDB(process.env.MONGO_INITDB_DATABASE);
+db.createUser({
+  user: process.env.MONGO_INITDB_ROOT_USERNAME,
+  pwd: process.env.MONGO_INITDB_ROOT_PASSWORD,
+  roles: [{ role: "readWrite", db: process.env.MONGO_INITDB_DATABASE }],
+});
+
+print("end ~~~~");
